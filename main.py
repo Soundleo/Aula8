6718df83fded6ef0aa14173e37086dec0788e25c
Soundleo <leorperes@gmail.com>
2024-05-21 19:48:49 -0300

    Aula 8 - laços de repetição - versão 1

---
+++ main.py
@@ -0,0 +1,8 @@
+# declaração de variável
+numero = int(input('Lazarento, informe um número qualquer.\n'))
+
+# loop
+while numero > -1:
+    print (numero)
+    numero -= 1
+print('fim do programa')
