5626ea99b5869158da8e0a1fb8a0e085715ed2a1
Soundleo <leorperes@gmail.com>
2024-05-21 21:06:07 -0300

    Aula 8 - versão 1.1

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+# importa bibliotecas
+import os 
+import time
+
 # declaração de variável
 numero = int(input('Lazarento, informe um número qualquer.\n'))
+os.system('cls')
+
 
 # loop
 while numero > -1:
-    print (numero)
+    print (f'Essa mensagem se auto destruirá em {numero}')
+    time.sleep(1)
+    os.system('cls')
     numero -= 1
-print('fim do programa')
+print('booommmm')
+
+########################################################################################################################
+
+#usar o continue 
+
+# cont = 0 
+
+# while cont < 10:
+    # cont += 1 
+    # if cont %2 == 0:
+        # print(cont)
+    # else: 
+        # continue
+    # print ('continuando')
+# 
+# print('ACABOU')
+        
